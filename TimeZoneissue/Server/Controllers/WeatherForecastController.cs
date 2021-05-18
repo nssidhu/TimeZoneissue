457f24d44603c510c6d7f01cf37a2cf6c3337872
Naunihal Singh Sidhu <nssidhu@outlook.com>
2021-05-18 10:32:27 -0500

    Add project files.

---
+++ TimeZoneissue/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeZoneissue.Shared;
+
+namespace TimeZoneissue.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var TimeZoneList = TimeZoneInfo.GetSystemTimeZones();
+
+            return Ok(TimeZoneList);
+           
+        }
+    }
+}
